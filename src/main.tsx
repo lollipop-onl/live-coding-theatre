42071cb47eaccf8620a0439468bcc64657e9ca81
simochee <lollipop@simochee.net>
2023-11-15 00:33:12 +0900

    ✨ setup react

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,5 @@
-console.log('application ready');
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root')!);
+
+root.render(<div>Hello World</div>);
