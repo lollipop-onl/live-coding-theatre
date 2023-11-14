3410c8f9ea4f62f53d8efc2c335dd961f57d9850
simochee <lollipop@simochee.net>
2023-11-15 00:54:25 +0900

    ✨ setup router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,50 @@
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
 
-const root = createRoot(document.getElementById('root')!);
 
-root.render(<div>Hello World</div>);
+const router = createBrowserRouter([
+  {
+    path: 'theatre/:uuid',
+    element: <Outlet />,
+    children: [
+      {
+        path: 'lobby',
+        element: <p>lobby</p>,
+      },
+      {
+        path: 'screen',
+        element: <p>screen</p>,
+      },
+      {
+        path: 'seat',
+        element: <p>seat</p>,
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    element: (
+      <p>
+        <Outlet />
+      </p>
+    ),
+    children: [
+      {
+        path: '',
+        element: <p>movie list</p>,
+      },
+      {
+        path: ':uuid',
+        element: <p>detail</p>,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <p>404</p>,
+  },
+]);
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<RouterProvider router={router} />);
