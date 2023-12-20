06ab49b8e3d43fa6706d1d91de029ad4f17fa28b
simochee <lollipop@simochee.net>
2023-12-20 17:57:57 +0900

    ⚡ fix list of audience

--- src/components/TheatreScreen.tsx
+++ src/components/TheatreScreen.tsx
@@ -67,7 +67,7 @@ export const TheatreScreen: React.FC = () => {
         </div>
         <div className="grow overflow-y-auto">
           <div className="grid md:grid-cols-2 lg:grid-cols-3">
-            {audiences.map((audience) => (
+            {audiences.slice().reverse().map((audience) => (
               <div key={audience.uuid} className="px-4 py-2">
                 <Seat
                   displayName={audience.displayName}
